b8ec7f79bbd9105422f8eb9d2fae5e77d7e2ca76
arthomnix <arthomnix@arthomnix.dev>
2023-11-30 10:55:45 +0000

    no rustfmt, you shouldn't order the days alphabetically

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,27 +1,76 @@
 #![allow(unused_variables)]
 
+#[rustfmt::skip]
 pub(crate) mod day1;
-pub(crate) mod day10;
-pub(crate) mod day11;
-pub(crate) mod day12;
-pub(crate) mod day13;
-pub(crate) mod day14;
-pub(crate) mod day15;
-pub(crate) mod day16;
-pub(crate) mod day17;
-pub(crate) mod day18;
-pub(crate) mod day19;
+
+#[rustfmt::skip]
 pub(crate) mod day2;
-pub(crate) mod day20;
-pub(crate) mod day21;
-pub(crate) mod day22;
-pub(crate) mod day23;
-pub(crate) mod day24;
-pub(crate) mod day25;
+
+#[rustfmt::skip]
 pub(crate) mod day3;
+
+#[rustfmt::skip]
 pub(crate) mod day4;
+
+#[rustfmt::skip]
 pub(crate) mod day5;
+
+#[rustfmt::skip]
 pub(crate) mod day6;
+
+#[rustfmt::skip]
 pub(crate) mod day7;
+
+#[rustfmt::skip]
 pub(crate) mod day8;
+
+#[rustfmt::skip]
 pub(crate) mod day9;
+
+#[rustfmt::skip]
+pub(crate) mod day10;
+
+#[rustfmt::skip]
+pub(crate) mod day11;
+
+#[rustfmt::skip]
+pub(crate) mod day12;
+
+#[rustfmt::skip]
+pub(crate) mod day13;
+
+#[rustfmt::skip]
+pub(crate) mod day14;
+
+#[rustfmt::skip]
+pub(crate) mod day15;
+
+#[rustfmt::skip]
+pub(crate) mod day16;
+
+#[rustfmt::skip]
+pub(crate) mod day17;
+
+#[rustfmt::skip]
+pub(crate) mod day18;
+
+#[rustfmt::skip]
+pub(crate) mod day19;
+
+#[rustfmt::skip]
+pub(crate) mod day20;
+
+#[rustfmt::skip]
+pub(crate) mod day21;
+
+#[rustfmt::skip]
+pub(crate) mod day22;
+
+#[rustfmt::skip]
+pub(crate) mod day23;
+
+#[rustfmt::skip]
+pub(crate) mod day24;
+
+#[rustfmt::skip]
+pub(crate) mod day25;
