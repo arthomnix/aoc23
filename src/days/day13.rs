c1dd459cfe3cfd25370b5a969f4b3e5219ffb333
arthomnix <arthomnix@arthomnix.dev>
2023-12-13 13:23:32 +0000

    improved 13:2

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -6,14 +6,16 @@ enum Reflection {
     Horizontal { row: usize },
 }
 
-fn find_reflection(grid: &Vec<Vec<bool>>, existing: Option<Reflection>) -> Option<Reflection> {
+fn find_reflection(grid: &Vec<Vec<bool>>, p2: bool) -> Option<Reflection> {
     for idx in 0..grid.len() - 1 {
-        let mut reflection = true;
+        let mut differences = 0;
         let mut upper = idx + 1;
         let mut lower = idx;
         loop {
-            if grid[upper] != grid[lower] {
-                reflection = false;
+            for x in 0..grid[0].len() {
+                if grid[upper][x] != grid[lower][x] {
+                    differences += 1;
+                }
             }
 
             if lower == 0 || upper == grid.len() - 1 {
@@ -23,22 +25,20 @@ fn find_reflection(grid: &Vec<Vec<bool>>, existing: Option<Reflection>) -> Optio
             lower -= 1;
             upper += 1;
         }
-        if reflection {
-            let r = Some(Horizontal { row: idx + 1 });
-            if r != existing {
-                return r;
-            }
+
+        if (!p2 && differences == 0) || (p2 && differences == 1) {
+            return Some(Horizontal { row: idx + 1 });
         }
     }
 
     for idx in 0..grid[0].len() - 1 {
-        let mut reflection = true;
+        let mut differences = 0;
         let mut upper = idx + 1;
         let mut lower = idx;
         loop {
             for y in 0..grid.len() {
                 if grid[y][upper] != grid[y][lower] {
-                    reflection = false;
+                    differences += 1;
                 }
             }
 
@@ -49,11 +49,9 @@ fn find_reflection(grid: &Vec<Vec<bool>>, existing: Option<Reflection>) -> Optio
             lower -= 1;
             upper += 1;
         }
-        if reflection {
-            let r = Some(Vertical { col: idx + 1 });
-            if r != existing {
-                return r;
-            }
+
+        if (!p2 && differences == 0) || (p2 && differences == 1) {
+            return Some(Vertical { col: idx + 1 });
         }
     }
 
@@ -73,9 +71,9 @@ fn parse_grids(input: String) -> Vec<Vec<Vec<bool>>> {
         .collect::<Vec<Vec<Vec<_>>>>()
 }
 
-pub(crate) fn part1(input: String) {
+pub(crate) fn day13(input: String, part2: bool) {
     let ans = parse_grids(input).iter().map(|grid| {
-        let reflection = find_reflection(grid, None).unwrap();
+        let reflection = find_reflection(grid, part2).unwrap();
         match reflection {
             Vertical { col: c } => c,
             Horizontal { row: r } => r * 100,
@@ -84,28 +82,10 @@ pub(crate) fn part1(input: String) {
     println!("{ans}");
 }
 
+pub(crate) fn part1(input: String) {
+    day13(input, false);
+}
+
 pub(crate) fn part2(input: String) {
-    let ans = parse_grids(input).iter().map(|grid| {
-        let smudged_reflection = find_reflection(grid, None).unwrap();
-        let mut real_reflection = None;
-        'outer: for y in 0..grid.len() {
-            for x in 0..grid[0].len() {
-                let mut desmudged = grid.clone();
-                desmudged[y][x] = !desmudged[y][x];
-                let reflection = find_reflection(&desmudged, Some(smudged_reflection));
-                if let Some(reflection) = reflection {
-                    if reflection != smudged_reflection {
-                        real_reflection = Some(reflection);
-                        break 'outer;
-                    }
-                }
-            }
-        }
-        let reflection = real_reflection.unwrap();
-        match reflection {
-            Vertical { col: c } => c,
-            Horizontal { row: r } => r * 100,
-        }
-    }).sum::<usize>();
-    println!("{ans}");
+    day13(input, true);
 }
